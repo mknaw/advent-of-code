677e72ae02c8c13e88df767c05a5649628e30677
mknaw <mknawara@gmail.com>
2022-12-13 19:45:34 -0500

    day 13

--- src/Lib/Utils.hs
+++ src/Lib/Utils.hs
@@ -2,6 +2,8 @@ module Lib.Utils
   ( applyToElem,
     bindN,
     boolToInt,
+    indicesWhere,
+    pairify,
     pairMap,
     trim,
   )
@@ -18,6 +20,11 @@ boolToInt False = 0
 bindN :: (Monad m, Integral n) => (a -> m a) -> n -> (a -> m a)
 bindN f n = foldr (>=>) return (replicate (fromIntegral n) f)
 
+-- | Convert length 2 list to a pair tuple
+pairify :: [a] -> (a, a)
+pairify [x, y] = (x, y)
+pairify _ = error "pairify: list must have exactly two elements"
+
 -- | Like <$> for a 2-tuple
 pairMap :: (a -> b) -> (a, a) -> (b, b)
 pairMap f (x, y) = (f x, f y)
@@ -32,3 +39,7 @@ applyToElem :: Int -> (a -> a) -> [a] -> [a]
 applyToElem n f xs = before ++ [f $ head after] ++ tail after
   where
     (before, after) = splitAt n xs
+
+-- | Get the indices of all elements that satisfy a predicate
+indicesWhere :: (a -> Bool) -> [a] -> [Int]
+indicesWhere f xs = map fst $ filter (f . snd) $ zip [0..] xs
